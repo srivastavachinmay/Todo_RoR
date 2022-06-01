0605256166a51504b24992273106be155a2f85b5
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2022-06-02 00:05:06 +0530

    update: added todo lists and todo items

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,12 +1,14 @@
 class TodoListsController < ApplicationController
-  before_action :set_todo_list, only: %i[ show edit update destroy ]
+  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
 
-  # GET /todo_lists or /todo_lists.json
+  # GET /todo_lists
+  # GET /todo_lists.json
   def index
     @todo_lists = TodoList.all
   end
 
-  # GET /todo_lists/1 or /todo_lists/1.json
+  # GET /todo_lists/1
+  # GET /todo_lists/1.json
   def show
   end
 
@@ -19,52 +21,54 @@ class TodoListsController < ApplicationController
   def edit
   end
 
-  # POST /todo_lists or /todo_lists.json
+  # POST /todo_lists
+  # POST /todo_lists.json
   def create
     @todo_list = TodoList.new(todo_list_params)
 
     respond_to do |format|
       if @todo_list.save
-        format.html { redirect_to todo_list_url(@todo_list), notice: "Todo list was successfully created." }
+        format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
         format.json { render :show, status: :created, location: @todo_list }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @todo_list.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /todo_lists/1 or /todo_lists/1.json
+  # PATCH/PUT /todo_lists/1
+  # PATCH/PUT /todo_lists/1.json
   def update
     respond_to do |format|
       if @todo_list.update(todo_list_params)
-        format.html { redirect_to todo_list_url(@todo_list), notice: "Todo list was successfully updated." }
+        format.html { redirect_to @todo_list, notice: 'Todo list was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo_list }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @todo_list.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /todo_lists/1 or /todo_lists/1.json
+  # DELETE /todo_lists/1
+  # DELETE /todo_lists/1.json
   def destroy
     @todo_list.destroy
-
     respond_to do |format|
-      format.html { redirect_to todo_lists_url, notice: "Todo list was successfully destroyed." }
+      format.html { redirect_to root_url, notice: 'Todo list was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo_list
-      @todo_list = TodoList.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def todo_list_params
-      params.require(:todo_list).permit(:title, :description)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def todo_list_params
+    params.require(:todo_list).permit(:title, :description)
+  end
 end
