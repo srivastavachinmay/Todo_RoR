539fa228589c5ab8d220a99e56e2c1ce0f53c072
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2022-06-02 19:27:59 +0530

    fix: delete button bug

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,6 +6,8 @@ class TodoItemsController < ApplicationController
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
   end
+  def show
+  end
 
   def destroy
     if @todo_item.destroy
